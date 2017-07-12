b9b36c8e4ea2f2120c70a599d17b0b349797f756
Alpha Chen <alpha.chen@gmail.com>
2017-07-12 08:04:06 -0700

    Split out views, add some CSS

--- presentations/present.rb
+++ presentations/present.rb
@@ -21,33 +21,6 @@ class Slide
 end
 
 class Present < Sinatra::Base
-  SLIDE_TEMPLATE = <<-ERB
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    body { margin: 0; padding-left: 2em; }
-  </style>
-</head>
-<body>
-  <div id="slide" class="<%= layout %>">
-    <%= doc.to_html %>
-  </div>
-  <script>
-    document.onkeydown = function(e) {
-        e = e || window.event;
-        if(e.key == ' ' || e.key == 'ArrowRight' || e.key == 'ArrowDown') {
-          document.location.href = "<%= n + 1 %>";
-        }
-        if(e.key == 'ArrowLeft' || e.key == 'ArrowUp') {
-          document.location.href = "<%= n - 1 %>";
-        }
-    };
-  </script>
-</body>
-</html>
-  ERB
-
   get "/" do
     redirect to("/0")
   end
@@ -55,16 +28,15 @@ class Present < Sinatra::Base
   get "/:n" do
     n = params["n"].to_i
     slide = settings.slides[n] || Slide.new(content: '')
-    content = slide.content || ''
+    content = slide.content
 
     doc = Kramdown::Document.new content, input: :GFM, syntax_highlighter: :rouge
 
-    erb SLIDE_TEMPLATE, locals: { doc: doc, n: n, layout: slide.layout }
+    erb :slide, locals: { doc: doc, n: n, layout: slide.layout }
   end
 end
 
 if __FILE__ == $0
-  # require 'pry'; binding.pry
   slides = YAML.load_file(ARGV.shift).flatten(1).map {|raw| Slide.from(raw) }
   Present.run! slides: slides
 end
