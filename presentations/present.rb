68b7d7e9078e56ec7a06f61d10654bc3038e6a11
Alpha Chen <alpha.chen@gmail.com>
2017-07-12 11:23:59 -0700

    Start working on slides

--- presentations/present.rb
+++ presentations/present.rb
@@ -16,10 +16,21 @@ class Slide
     end
   end
 
-  attr_reader *%i[ content layout ]
+  attr_reader *%i[ content layout increments notes ]
 
-  def initialize(content:, layout: :default)
-    @content, @layout = content, layout
+  def initialize(content:, layout: nil, increments: nil, notes: nil)
+    @content = content || ''
+    @layout = layout || :default
+    @increments = increments || []
+    @notes = notes || ''
+  end
+
+  def to_a
+    increments.each.with_object([content]) {|i, a|
+      a << [a.last, "- #{i}"].join("\n")
+    }.map {|c|
+      Slide.new(content: c, layout: layout, notes: notes)
+    }
   end
 end
 
@@ -40,6 +51,6 @@ class Present < Sinatra::Base
 end
 
 if __FILE__ == $0
-  slides = YAML.load_file(ARGV.shift).flatten(1).map {|raw| Slide.from(raw) }
+  slides = YAML.load_file(ARGV.shift).flat_map {|raw| Slide.from(raw).to_a }
   Present.run! slides: slides
 end
